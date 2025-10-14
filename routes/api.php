5fdd7b26272f17c49b9645c1ea0840cc17389d25
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2025-10-14 10:29:17 +0700

    Add route for products endpoint using ProductController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/halo', function () {
     return 'halo, laravel!';
 });
+
+Route::get('/products', [ProductController::class, 'index']);
