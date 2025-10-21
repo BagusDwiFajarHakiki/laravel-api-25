442b45be26840dd21dd3884b777454d7c175e157
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2025-10-21 08:50:08 +0700

    Add new 'halo' route and update products routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,9 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/halo', function () {
+    return 'Halo, Laravel';
+});
+
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
