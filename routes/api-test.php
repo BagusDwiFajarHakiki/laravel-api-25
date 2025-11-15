88e93da6aa6ed068d6d01d0f418a214074569be8
BagusDwiFajarHakiki <bagusdwifajarhakiki18@gmail.com>
2025-11-15 08:45:06 +0700

    Refactor ProductController namespace to improve organization by moving it under the Api directory.

--- routes/api-test.php
+++ routes/api-test.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
